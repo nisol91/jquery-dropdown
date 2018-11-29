b0fceb1e30d442ecd48cb38832e0344bfbe1b8a4
nisol91 <nicolasolzi91@gmail.com>
2018-11-29 17:58:12 +0100

    3

--- main.js
+++ main.js
@@ -1,16 +1,54 @@
+//PRIMO LIVELLO-------
+
+//COMPORTAMENTO QUANDO SI PUNTA IL MOUSE-------
+
 $('.nav-el').mouseenter(function() {
   $(this).children('a').children('h2').addClass('black-text');
 });
-$('.nav-el').mouseleave(function() {
-  $(this).children('a').children('h2').removeClass('black-text');
+$('.nav-el').mouseenter(function() {
+  $(this).children('.drop-elem').slideDown();
 });
 
-$('.nav-el').click(function() {
-  $('.nav-el').removeClass('bianco');
-  $(this).addClass('bianco');
-  $(this).children('a').children('h2').addClass('black-text');
-  if (true) {
-    $(this).children('.drop-elem').slideDown();
-  }
+//COMPORTAMENTO QUANDO SI TOGLIE IL MOUSE-------
 
+$('.nav-el').mouseleave(function() {
+  if ($(this).hasClass('open')) {//se ha la classe open e me ne vado via col mouse, chiude il dropdown (molto simile al comportamento al secondo click)
+    $(this).children('.drop-elem').slideUp();
+    $(this).removeClass('open');//ovviamente la devo rimuovere per permettere di riaprirlo successivamente
+    $(this).children('a').children('h2').removeClass('black-text');//il testo torna bianco
+  } else {
+  $(this).children('a').children('h2').removeClass('black-text');//il testo torna bianco
+  $(this).children('.drop-elem').slideUp();//se tolgo il mouse dal div che contiene testo, bottone e drop, il drop si chiude
+  $(this).removeClass('open');//gli do la classe open, che mi serve per la parte in alto con l if
+  }
+});
+
+//COMPORTAMENTO AL CLICK-------
+
+$('.nav-el').click(function() {
+  $(this).children('.drop-elem').slideUp();
+
+  // if ($(this).hasClass('open')) {//questo if e' per richiudere il menu ricliccando sullo stesso, grazie alla class fittizia 'open' che ho dato.
+  //   $(this).children('.drop-elem').slideUp();//ovvero: se il menu e' aperto e riclicco, si chiude
+  //   $(this).removeClass('open');//ovviamente poi la rimuovo dopo il controllo if, se no non mi riapre piu il dropdown
+  // } else {
+  //   $(this).children('.drop-elem').slideDown();//lo faccio comparire con uno slidedown
+  //   $(this).addClass('open');//gli do la classe open cosi se la ha quando riclicco mi chiude il menu (vedi parte in alto con l if)
+  // }
+});
+
+//-------------------------------------------------
+//SECONDO LIVELLO--------------
+
+$('.drop-elem-i').mouseenter(function() {
+  $(this).children('a').children('h2').addClass('black-text');
+  $(this).children('a').children('h2').children('i').addClass('black-text');
+});
+$('.drop-elem-i').mouseleave(function() {
+  $(this).children('a').children('h2').removeClass('black-text');
+  $(this).children('a').children('h2').children('i').addClass('black-text');
+});
+
+$('.drop-elem-i').mouseenter(function() {
+  $(this).children('.drop-elem-ii').slideDown();
 });
